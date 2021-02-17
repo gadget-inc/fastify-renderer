14d4db0f3a636947499c31f642e0ec33c2a379a6
Harry Brundage <harry.brundage@gmail.com>
2021-02-17 18:33:05 -0500

    Whoopth

--- src/client/fetcher.ts
+++ src/client/fetcher.ts
@@ -6,18 +6,21 @@ interface Tracker<T> {
 
 const trackers: Record<string, Tracker<any>> = {}
 
+/**
+ * Implements a React Suspense cache for a promise. For each passed `key` passed, runs the promise on first invocation, stores it, and suspends. On the next invocaation, will synchronously return the result of the promise if it resolved, or throw it's reject reason if it rejected.
+ **/
 export const usePromise = <T>(key: string, promise: () => Promise<T>) => {
   let tracker = trackers[key]
 
   if (tracker) {
     // If an error occurred,
-    if (Object.prototype.hasOwnProperty.call(trackers, 'error')) {
-      throw trackers.error
+    if (Object.prototype.hasOwnProperty.call(tracker, 'error')) {
+      throw tracker.error
     }
 
     // If a response was successful,
-    if (Object.prototype.hasOwnProperty.call(trackers, 'response')) {
-      return trackers.response
+    if (Object.prototype.hasOwnProperty.call(tracker, 'response')) {
+      return tracker.response
     }
   }
 
@@ -25,10 +28,10 @@ export const usePromise = <T>(key: string, promise: () => Promise<T>) => {
     tracker = {
       promise: promise()
         .then((response: any) => {
-          trackers.response = response
+          tracker.response = response
         })
         .catch((e: any) => {
-          trackers.error = e
+          tracker.error = e
         }),
     }
 
