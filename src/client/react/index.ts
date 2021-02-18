64e715e52eb3490ee3090e366b7d6004a4f781bf
Harry Brundage <harry.brundage@gmail.com>
2021-02-18 18:33:21 -0500

    0.1.8

--- src/client/react/index.ts
+++ src/client/react/index.ts
@@ -1,4 +1,4 @@
-export { Link, Redirect, Route, Switch, useLocation, useRoute, useRouter } from 'wouter'
+export { Link, Redirect, Route, Router, Switch, useLocation, useRoute, useRouter } from 'wouter'
 export { useTransitionLocation } from './locationHook'
 export { Root } from './Root'
 export type { LayoutProps } from './Root'
