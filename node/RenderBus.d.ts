a1484041894d80ed91d92a23c107e629a9159c90
Harry Brundage <harry.brundage@gmail.com>
2021-02-27 09:17:35 -0500

    gitpkg

---
+++ node/RenderBus.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="react" />
+/** Holds groups of content during a render that eventually get pushed into the template. */
+export declare class RenderBus {
+    static Context: import("react").Context<RenderBus>;
+    stacks: Record<string, string[]>;
+    included: Set<string>;
+    push(key: string, content: string): void;
+    stack(key: any): string[];
+    preloadModule(path: string): void;
+    linkStylesheet(path: string): void;
+}
