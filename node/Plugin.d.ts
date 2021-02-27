2efaa1a4815a9d0fbadc979d3bda5d086b79dedd
Harry Brundage <harry.brundage@gmail.com>
2021-02-27 09:34:23 -0500

    gitpkg

---
+++ node/Plugin.d.ts
@@ -0,0 +1,41 @@
+import 'middie';
+import { InlineConfig } from 'vite';
+import { Template } from './DocumentTemplate';
+import { RenderBus } from './RenderBus';
+import { ReactRendererOptions } from './renderers/react/ReactRenderer';
+import { Renderer } from './renderers/Renderer';
+import './types';
+import { FastifyRendererHook, ViteClientManifest, ViteServerManifest } from './types';
+export interface FastifyRendererOptions {
+    renderer?: ReactRendererOptions;
+    vite?: InlineConfig;
+    layout?: string;
+    document?: Template;
+    devMode?: boolean;
+    outDir?: string;
+    assetsHost?: string;
+    hooks?: (FastifyRendererHook | (() => FastifyRendererHook))[];
+}
+export declare class FastifyRendererPlugin {
+    renderer: Renderer;
+    devMode: boolean;
+    vite?: InlineConfig;
+    base: string;
+    outDir: string;
+    layout: string;
+    document: Template;
+    assetsHost: string;
+    hooks: FastifyRendererHook[];
+    clientManifest?: ViteClientManifest;
+    serverManifest?: ViteServerManifest;
+    constructor(incomingOptions: FastifyRendererOptions);
+    /**
+     * For a vite module id, returns the path it will be accessible at from the browser.
+     * Adds in the `base`, and the `assetsHost` if it exists
+     */
+    clientAssetPath(asset: string): string;
+    /**
+     * Implements the backend integration logic for vite -- pulls out the chain of imported modules from the vite manifest and generates <script/> or <link/> tags to source the assets in the browser.
+     **/
+    pushImportTagsFromManifest: (bus: RenderBus, entryName: string, root?: boolean) => void;
+}
---
+++ node/renderers/Renderer.d.ts
@@ -0,0 +1,16 @@
+import { FastifyReply, FastifyRequest, RouteOptions } from 'fastify';
+import { Plugin, ResolvedConfig, ViteDevServer } from 'vite';
+/** A unit of renderable work */
+export interface Render<Props> {
+    request: FastifyRequest;
+    reply: FastifyReply;
+    props: Props;
+    renderable: string;
+}
+/** An object that knows how to render */
+export interface Renderer {
+    prepare(routes: RouteOptions[], viteOptions: ResolvedConfig, devServer?: ViteDevServer): Promise<void>;
+    render<Props>(render: Render<Props>): Promise<void>;
+    buildVirtualClientEntrypointModuleURL(id: string): string;
+    vitePlugins(): Plugin[];
+}
