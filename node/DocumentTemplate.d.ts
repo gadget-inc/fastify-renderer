aa12e1d996097b4937f456f4bebd61dbb31a6793
Harry Brundage <harry.brundage@gmail.com>
2021-03-02 17:27:02 -0500

    gitpkg

---
+++ node/DocumentTemplate.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="node" />
+/** Data passed to the template function by the renderer */
+export interface TemplateData<Props> {
+    head: string;
+    tail: string;
+    content: string | NodeJS.ReadableStream;
+    props: Props;
+}
+/** A template renders out a full HTML document given the content for the document and the scripts for the document, and can optionally grab values out of the props to use for other bits like the page title, metatags, or non-client-side-hydrated body content. */
+export declare type Template = (data: TemplateData<any>) => NodeJS.ReadableStream;
+export declare const DefaultDocumentTemplate: Template;
