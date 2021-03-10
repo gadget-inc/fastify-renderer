6b4ed112b97df5f438eddf8c48859f2714805007
Harry Brundage <harry.brundage@gmail.com>
2021-03-10 08:45:01 -0500

    gitpkg

---
+++ client/react/locationHook.d.ts
@@ -0,0 +1,4 @@
+export declare const events: string[];
+export declare const useTransitionLocation: ({ base }?: {
+    base?: string | undefined;
+}) => (string | boolean | ((to: any, { replace }?: any) => void))[];
