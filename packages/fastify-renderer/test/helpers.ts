0ba9f6008f6efba5a373d273b99cc8a2630026de
Ayoub El Khattabi <elkhattabi.ayoub@gmail.com>
2021-07-05 13:35:45 +0100

    add RenderBus tests

--- packages/fastify-renderer/test/helpers.ts
+++ packages/fastify-renderer/test/helpers.ts
@@ -1,10 +1,16 @@
 import fastify, { FastifyServerOptions } from 'fastify'
 import fastifyAccepts from 'fastify-accepts'
 import Middie from 'middie'
+import { RenderBus } from '../src/node/RenderBus'
+import { ReactRenderer, ReactRendererOptions } from '../src/node/renderers/react/ReactRenderer'
 
 export const newFastify = async (options?: FastifyServerOptions) => {
   const server = fastify(options)
   await server.register(fastifyAccepts)
   await server.register(Middie)
   return server
+}
+
+export const newRenderBus = () => {
+  return new RenderBus()
 }
