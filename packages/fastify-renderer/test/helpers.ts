369f9ac9677c1b03b68eb1b9596da81a9e0b7586
Ayoub El Khattabi <elkhattabi.ayoub@gmail.com>
2021-07-05 13:36:53 +0100

    add FastifyRenderer tests

--- packages/fastify-renderer/test/helpers.ts
+++ packages/fastify-renderer/test/helpers.ts
@@ -1,6 +1,7 @@
 import fastify, { FastifyServerOptions } from 'fastify'
 import fastifyAccepts from 'fastify-accepts'
 import Middie from 'middie'
+import { FastifyRendererOptions, FastifyRendererPlugin } from '../src/node/Plugin'
 import { RenderBus } from '../src/node/RenderBus'
 import { ReactRenderer, ReactRendererOptions } from '../src/node/renderers/react/ReactRenderer'
 
@@ -14,3 +15,8 @@ export const newFastify = async (options?: FastifyServerOptions) => {
 export const newRenderBus = () => {
   return new RenderBus()
 }
+
+export const newFastifyRendererPlugin = (options: FastifyRendererOptions = {}) => {
+  return new FastifyRendererPlugin(options)
+}
+
