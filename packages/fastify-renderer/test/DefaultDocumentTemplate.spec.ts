b48f1802f9b53074e07ed964792fff3919cb77e6
Ayoub El Khattabi <elkhattabi.ayoub@gmail.com>
2021-07-05 13:39:45 +0100

    add DefaultDocumentTemplate test file

---
+++ packages/fastify-renderer/test/DefaultDocumentTemplate.spec.ts
@@ -0,0 +1,7 @@
+// import { DefaultDocumentTemplate } from "../src/node/DocumentTemplate";
+
+describe('DefaultDocumentTemplate', () => {
+  test.skip('should correctly stream the html code', async () => {
+    
+  });
+})
