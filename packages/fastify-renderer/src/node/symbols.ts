7550a3fd85ed42bfa344ed83d8f84a415db52bff
Ayoub El Khattabi <elkhattabi.ayoub@gmail.com>
2021-07-05 13:37:59 +0100

    extract symbols to separate file to be usable for tests

---
+++ packages/fastify-renderer/src/node/symbols.ts
@@ -0,0 +1,3 @@
+export const kRendererPlugin = Symbol('fastify-renderer-plugin')
+export const kRenderOptions = Symbol('fastify-renderer-render-options')
+export const kRendererViteOptions = Symbol('fastify-renderer-vite-options')
