7cca9977a5c559c8290f0065d0e23d1e7b75bcf0
Ayoub El Khattabi <elkhattabi.ayoub@gmail.com>
2021-08-07 10:55:51 +0100

    wrap RedLayout in a Suspense component

--- packages/test-apps/simple-react/RedLayout.tsx
+++ packages/test-apps/simple-react/RedLayout.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const Layout = (props: { children: React.ReactNode }) => {
-  return <div style={{ backgroundColor: 'rgba(100, 0, 0, 0.4)' }}>{props.children}</div>
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <div style={{ backgroundColor: 'rgba(100, 0, 0, 0.4)' }}>{props.children}</div>
+    </Suspense>
+  )
 }
 
 export default Layout
