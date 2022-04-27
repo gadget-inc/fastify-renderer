45ff8f78f4d82b2a3cdb02cea5f68214fab9687e
Ayoub El Khattabi <elkhattabi.ayoub@gmail.com>
2022-04-27 23:23:07 +0000

    add test that makes sure the imperative component is included in the route table

--- packages/test-apps/simple-react/test/imperative-rendering.spec.ts
+++ packages/test-apps/simple-react/test/imperative-rendering.spec.ts
@@ -1,5 +1,5 @@
 import { Page } from 'playwright-chromium'
-import { newTestPage, rootURL } from '../../helpers'
+import { newTestPage, reactReady, rootURL } from '../../helpers'
 
 // For these tests, we don't use reactReady because the page is rendered server side so we don't need to wait on the client side
 describe('imperative rendering', () => {
@@ -9,25 +9,29 @@ describe('imperative rendering', () => {
     page = await newTestPage()
   })
 
-  test('the route should not fetch props and hydrate on the client side', async () => {
-    page.on('request', (request) => {
-      if (request.headers().accept === 'application/json') {
-        throw new Error(
-          `Expecting request to only be rendered server side, props request made: ${request.method()} ${request.url()}`
-        )
+  test('the route table should include the imperative route', async () => {
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    page.on('response', async (response) => {
+      if (response.url().includes('@fstr!route-table.js')) {
+        const responseBody = await response.text()
+        expect(responseBody).toBeDefined()
+        expect(responseBody.includes('/imperative/true')).toBe(true)
       }
     })
 
-    await page.goto(`${rootURL}/imperative-true`)
+    await page.goto(`${rootURL}/imperative/true`)
+    await reactReady(page)
   })
 
   test('the route handler should redirect to a 404 page', async () => {
     await page.goto(`${rootURL}/imperative/false`)
+    await reactReady(page)
     expect(await page.isVisible("text='Not Found'")).toBe(true)
   })
 
   test('the route handler should render the component', async () => {
     await page.goto(`${rootURL}/imperative/true`)
+    await reactReady(page)
     expect(await page.isVisible("text='Imperatively renderable page'")).toBe(true)
   })
 })
