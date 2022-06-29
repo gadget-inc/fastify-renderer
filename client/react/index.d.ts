77e4ab249a301e6f5576d5da7d7fdb9a7215813a
Harry Brundage <harry.brundage@gmail.com>
2022-06-29 14:16:20 -0400

    gitpkg

---
+++ client/react/index.d.ts
@@ -0,0 +1,10 @@
+export { Link, Redirect, Route, Router, Switch, useLocation, useRoute, useRouter } from 'wouter';
+export { useNavigationDetails, useTransitionLocation } from './locationHook';
+export { Root } from './Root';
+export type { LayoutProps } from './Root';
+export declare const RenderBusContext: import("react").Context<any>;
+declare global {
+    interface Window {
+        fastifyRendererReady?: boolean;
+    }
+}
