a2e846c087937a1e665fb02acbc90d48c3f4caee
Harry Brundage <harry.brundage@gmail.com>
2022-11-22 16:52:20 -0500

    Upgrade some development dependencies to see if vite upgrade segfault is fixed

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Gadget development environment";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    (flake-utils.lib.eachSystem [
+      "x86_64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ]
+      (system: nixpkgs.lib.fix (flake:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          packages =
+            rec {
+              git = pkgs.git;
+
+              nix-direnv = pkgs.nix-direnv.override {
+                enableFlakes = true;
+              };
+
+              nodejs = pkgs.nodejs-16_x;
+
+              yarn = pkgs.yarn.override {
+                inherit nodejs;
+              };
+            };
+
+          devShell = pkgs.mkShell {
+            packages = builtins.attrValues packages;
+          };
+        }
+      )));
+
+  nixConfig.bash-prompt = "\[fstr:\\w\]$ ";
+}
