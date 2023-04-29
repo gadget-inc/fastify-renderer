aef42973030801def774ec76738eabbfeeca95e8
Slava Knyazev <slava@knyazev.io>
2023-04-29 00:34:38 +0000

    Remove Worker.js

--- packages/fastify-renderer/src/node/renderers/react/Worker.import.js
+++ packages/fastify-renderer/src/node/renderers/react/Worker.import.js
@@ -1,3 +1,6 @@
 // Context: https://github.com/TypeStrong/ts-node/issues/676#issuecomment-470898116
-require('ts-node').register();
+require('ts-node').register({
+    // Disable type-checking
+    transpileOnly: true
+});
 require(require.resolve('./Worker.ts'));
