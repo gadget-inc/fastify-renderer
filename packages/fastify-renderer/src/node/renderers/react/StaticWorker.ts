20df079c2e6213d4bb748efe51113147ed4a0d01
Slava Knyazev <slava@knyazev.io>
2023-05-05 17:13:51 +0000

    One more ignore

--- packages/fastify-renderer/src/node/renderers/react/StaticWorker.ts
+++ packages/fastify-renderer/src/node/renderers/react/StaticWorker.ts
@@ -6,6 +6,7 @@ if (!parentPort) throw new Error('Missing parentPort')
 const port = parentPort
 
 port.on('message', (args: WorkerRenderInput) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const content = staticRender({ ...args, module: require(args.modulePath).default })
   port.postMessage(content)
 })
