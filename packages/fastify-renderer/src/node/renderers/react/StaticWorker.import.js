f9b6aa5920485ebd9882820f66c749ed144aa879
Slava Knyazev <slava@knyazev.io>
2023-05-16 19:45:40 +0000

    Try-catch load ts-node

--- packages/fastify-renderer/src/node/renderers/react/StaticWorker.import.js
+++ packages/fastify-renderer/src/node/renderers/react/StaticWorker.import.js
@@ -1,7 +1,14 @@
-// Context: https://github.com/TypeStrong/ts-node/issues/676#issuecomment-470898116
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('ts-node').register({
-  // Disable type-checking
-  transpileOnly: true,
-})
-require(require.resolve('./StaticWorker.ts'))
+try {
+  const tsWorkerPath = require.resolve('./StaticWorker.ts')
+
+  // Context: https://github.com/TypeStrong/ts-node/issues/676#issuecomment-470898116
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('ts-node').register({
+    // Disable type-checking
+    transpileOnly: true,
+  })
+  require(tsWorkerPath)
+} catch {
+  const jsWorkerPath = require.resolve('./StaticWorker.js')
+  require(jsWorkerPath)
+}
--- packages/fastify-renderer/src/node/renderers/react/StreamingWorker.import.js
+++ packages/fastify-renderer/src/node/renderers/react/StreamingWorker.import.js
@@ -1,7 +1,14 @@
-// Context: https://github.com/TypeStrong/ts-node/issues/676#issuecomment-470898116
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('ts-node').register({
-  // Disable type-checking
-  transpileOnly: true,
-})
-require(require.resolve('./StreamingWorker.ts'))
+try {
+  const tsWorkerPath = require.resolve('./StreamingWorker.ts')
+
+  // Context: https://github.com/TypeStrong/ts-node/issues/676#issuecomment-470898116
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('ts-node').register({
+    // Disable type-checking
+    transpileOnly: true,
+  })
+  require(tsWorkerPath)
+} catch {
+  const jsWorkerPath = require.resolve('./StreamingWorker.js')
+  require(jsWorkerPath)
+}
