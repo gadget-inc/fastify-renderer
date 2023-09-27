d9d7c0c8f5fed5c0a4a03e7c2fae822ad4a2f561
Harry Brundage <harry.brundage@gmail.com>
2023-09-26 23:02:12 -0400

    gitpkg

---
+++ node/symbols.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.kRendererViteOptions = exports.kRenderOptions = exports.kRendererPlugin = void 0;
+exports.kRendererPlugin = Symbol('fastify-renderer-plugin');
+exports.kRenderOptions = Symbol('fastify-renderer-render-options');
+exports.kRendererViteOptions = Symbol('fastify-renderer-vite-options');
+//# sourceMappingURL=symbols.js.map
