a5fab24188227d551c30041f4081228460c2ac8a
Slava Knyazev <slava@knyazev.io>
2023-11-03 15:55:07 +0000

    lint

--- packages/fastify-renderer/src/node/DocumentTemplate.tsx
+++ packages/fastify-renderer/src/node/DocumentTemplate.tsx
@@ -20,8 +20,7 @@ export interface TemplateData<Props> {
 /** A template renders out a full HTML document given the content for the document and the scripts for the document, and can optionally grab values out of the props to use for other bits like the page title, metatags, or non-client-side-hydrated body content. */
 export type Template = (data: TemplateData<any>) => NodeJS.ReadableStream
 
-export const DefaultDocumentTemplate: Template = (data: TemplateData<any>) => {
-  const result = template`
+export const DefaultDocumentTemplate: Template = (data: TemplateData<any>) => template`
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -36,7 +35,3 @@ export const DefaultDocumentTemplate: Template = (data: TemplateData<any>) => {
   </body>
 </html>
 `
-  result.pipe(process.stderr)
-  // console.log(result)
-  return result
-}
